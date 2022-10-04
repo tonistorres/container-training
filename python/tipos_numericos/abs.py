d469831d4e473c8bcac3af2e48e9beae7ae75bae
tonistorres <sisvenda2011@gmail.com>
2022-10-03 21:54:26 -0300

    feat: implementing function abs_function_built_in

---
+++ python/tipos_numericos/abs.py
@@ -0,0 +1,15 @@
+# Retorna o valor absoluto de um número. O argumento pode
+# ser um inteiro, um número de ponto flutuante.
+
+# Traduzindo: Mesmo que o resultado seja um numero negativo
+# ele é convertido em positivo pela função abs
+
+
+def abs_function_built_in(number):
+    result = abs(number)
+    print(result)
+
+
+abs_function_built_in(-10.45)
+
+abs_function_built_in(-220)
