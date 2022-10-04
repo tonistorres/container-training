88519de5770948b054085be115ffed1b7261c49c
tonistorres <sisvenda2011@gmail.com>
2022-10-03 21:54:39 -0300

    feat: casting_int

---
+++ python/tipos_numericos/casting.py
@@ -0,0 +1,14 @@
+def casting_int(number):
+    restul_casting = int(number)
+    print(restul_casting)
+
+
+casting_int(23.45)
+
+
+def casting_float(number):
+    result_casting_float = float(number)
+    print(f'Casting Float: {result_casting_float}')
+
+
+casting_float(12)
