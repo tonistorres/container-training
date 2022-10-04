69ca40f4ddcf4238e7620b638b14adfc0f6245a4
tonistorres <sisvenda2011@gmail.com>
2022-10-04 20:28:18 -0300

    feat: implementing function enter_str less than

--- python/operadores_comparacao/operator_less_than.py
+++ python/operadores_comparacao/operator_less_than.py
@@ -1,9 +1,13 @@
 # Operador Menor que: <
 # Desenvolva uma função que faça uso desse operador.
+# https://docs.python.org/pt-br/3/library/stdtypes.html#boolean-operations-and-or-not
 
 
-def enter_str(str):
-    print(len(str))
+def enter_str(str_one, str_two):
+    if len(str_one) < len(str_two):
+        print("a string one menor que string two")
+    else:
+        print("a string two menor que string one")
 
 
-enter_str("tonis")
+enter_str("tonis", "alberto")
