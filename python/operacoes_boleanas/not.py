b5370467e279f114a04b61aa8ce23cdfeaa76a9a
tonistorres <sisvenda2011@gmail.com>
2022-10-04 09:47:49 -0300

    feat: implementing function enter_one_number

---
+++ python/operacoes_boleanas/not.py
@@ -0,0 +1,11 @@
+# Utilizando o operador NOT se x igual a 10: imprima "not é 10", ou seja, negue a operação
+
+
+def enter_one_number(number_a):
+    if not number_a == 10:
+        print("o comando not me arremessou para o else ):")
+    else:
+        print("vem pra k nene :)")
+
+
+enter_one_number(10)
