9e67fdee4d698fa0d05246d5858fe4f17aa56d81
tonistorres <sisvenda2011@gmail.com>
2022-10-04 09:16:55 -0300

    refactor: adding link doc python

--- python/tipos_numericos/casting.py
+++ python/tipos_numericos/casting.py
@@ -5,10 +5,12 @@ def casting_int(number):
 
 casting_int(23.45)
 
+# https://docs.python.org/pt-br/3/library/stdtypes.html
+
 
 def casting_float(number):
     result_casting_float = float(number)
-    print(f'Casting Float: {result_casting_float}')
+    print(f"Casting Float: {result_casting_float}")
 
 
 casting_float(12)
