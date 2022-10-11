880a1f962bb2c60783e4e8432412653dde7e50a6
tonistorres <sisvenda2011@gmail.com>
2022-10-11 09:49:25 -0300

    feat: implementing function square

---
+++ python/python_calculadora/ square.py
@@ -0,0 +1,9 @@
+# criar uma função que calcule a raiz quadra de um número
+
+
+def square(number, expoent):
+    result = number**expoent
+    print("O resultado da potenciação é: {}".format(result))
+
+
+square(2, 2)
