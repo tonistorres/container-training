7b128413a44909ab9e8aa0f0ad610acb1f130e4b
tonistorres <sisvenda2011@gmail.com>
2022-10-11 19:57:12 -0300

    refactor: deleting files not used

--- python/software_iniciante-desafio/desafio_pratico.py
+++
@@ -1,8 +0,0 @@
-def convert():
-    value_reais = float(input("Digite o valor em Reais a ser convertido R$:"))
-    dollar_count_value = float(input("Digite o valor da contação do Dólar U$:"))
-    result = value_reais / dollar_count_value
-    print("O valor em Dólar é U$ {}".format(result))
-
-
-convert()
