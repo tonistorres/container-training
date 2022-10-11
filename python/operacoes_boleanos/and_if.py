ef4c8eaebafdaf42828b3a2319c616599c405a94
tonistorres <sisvenda2011@gmail.com>
2022-10-11 19:59:46 -0300

    feat: implementing function enter_tow_number

---
+++ python/operacoes_boleanos/and_if.py
@@ -0,0 +1,11 @@
+# Se x e z são iguais: imprima "somos iguais" caso contrário "xablau"
+
+
+def enter_two_number(number_a, number_b):
+    if number_a == number_b:
+        print("são iguais")
+    else:
+        print("xablau")
+
+
+enter_two_number(10, 10)
