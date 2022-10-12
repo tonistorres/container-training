ac400e90303acfc371d86ff425455f9ba4a68493
tonistorres <sisvenda2011@gmail.com>
2022-10-12 19:12:28 -0300

    feat: implementing file estrutura_decisao_composta.py

---
+++ python/estrutura_decisao_if/estrutura_decisao_composta.py
@@ -0,0 +1,8 @@
+# crie uma função que possibilita se uma pessoa pode dirigir uma veículo
+
+idade = int(input("Digite sua Idade:"))
+
+if idade < 18:
+    print("Você não tem idade suficiente para dirigir um veiculo")
+else:
+    print("Seja bem vindo tenha uma boa viagem.")
