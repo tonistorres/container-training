75fda57d71f023edbb0db691846cc863a9778ca0
tonistorres <sisvenda2011@gmail.com>
2022-10-15 16:40:41 -0300

    feat: implementing operation with list

---
+++ python/tipos_embutidos/randon.py
@@ -0,0 +1,9 @@
+# Python não tem uma função random() para fazer um número aleatório,
+# mas Python tem um módulo embutido chamado random que pode ser usado
+#  para fazer números aleatórios:
+
+# Importe o módulo aleatório e exiba um número aleatório entre 1 e 9:
+
+import random
+
+print(random.randrange(1, 10))
