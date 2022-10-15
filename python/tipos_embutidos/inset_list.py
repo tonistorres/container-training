aa8627ce3d9ceca0def65851b7d770e9a04bb22c
tonistorres <sisvenda2011@gmail.com>
2022-10-15 16:39:36 -0300

    feat: implementing operation with list

---
+++ python/tipos_embutidos/inset_list.py
@@ -0,0 +1,9 @@
+"""
+Para inserir um novo item de lista, sem substituir nenhum dos
+ valores existentes, podemos usar o método insert().
+O método insert() insere um item no índice especificado:
+"""
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
