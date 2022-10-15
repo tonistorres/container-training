b15a2dc534ddd53d6f7d7a5974f854e09b92db25
tonistorres <sisvenda2011@gmail.com>
2022-10-15 16:40:04 -0300

    feat: implementing operation with list

---
+++ python/tipos_embutidos/list_check_exist.py
@@ -0,0 +1,9 @@
+"""
+Verifique se o item existe
+Para determinar se um item especificado está presente em uma lista,
+ use a palavra-chave in
+"""
+
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+    print("Yes, 'apple' is in the fruits list")
