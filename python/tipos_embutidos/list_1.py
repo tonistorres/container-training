118a8c9a3dcfcb7f694e35add619abf9c872df8f
tonistorres <sisvenda2011@gmail.com>
2022-10-15 16:39:42 -0300

    feat: implementing operation with list

---
+++ python/tipos_embutidos/list_1.py
@@ -0,0 +1,10 @@
+# Lista no python visualmente é muito parecido com o array do javascript
+# porém por debaixo dos panos as coisas funcionam diferente algortmos
+# diferentes mais para que usa é a mesma dinâmica
+
+# Exemplo: Dado A lista abaixo acesse seus elementos pelo indice:
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[0])
+print(thislist[1])
+print(thislist[2])
