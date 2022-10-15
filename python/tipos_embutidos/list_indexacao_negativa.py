5f70d4cb19b26cc2628fb93cc6b6ce5803312dc7
tonistorres <sisvenda2011@gmail.com>
2022-10-15 16:40:13 -0300

    feat: implementing operation with list

---
+++ python/tipos_embutidos/list_indexacao_negativa.py
@@ -0,0 +1,8 @@
+# Indexação negativa
+# Indexação negativa significa começar do fim
+# -1 refere-se ao último item,
+#  -2 refere-se ao penúltimo item etc.
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+print(thislist[-2])
