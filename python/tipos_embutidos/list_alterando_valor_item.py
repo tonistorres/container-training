a834c7fd279eab22887c0ca8cca432e3a0873320
tonistorres <sisvenda2011@gmail.com>
2022-10-15 16:39:57 -0300

    feat: implementing operation with list

---
+++ python/tipos_embutidos/list_alterando_valor_item.py
@@ -0,0 +1,8 @@
+"""
+Alterar valor do item
+Para alterar o valor de um item específico, consulte o número do índice
+"""
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
