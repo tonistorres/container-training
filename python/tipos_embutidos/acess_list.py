d643288df27a9c4867c398693efa89bac258ee2f
tonistorres <sisvenda2011@gmail.com>
2022-10-15 16:39:19 -0300

    feat: implementing operation with list

---
+++ python/tipos_embutidos/acess_list.py
@@ -0,0 +1,5 @@
+# Os itens da lista são indexados e você pode acessá-los consultando
+# o número do índice:
+thislist = ["apple", "banana", "cherry"]
+
+print("Acessando o primeiro elemento da lista:[{}]".format(thislist[0]))
