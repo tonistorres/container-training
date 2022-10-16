66531ea62d35e9c3aa88d60212156750a5ede9b1
tonistorres <sisvenda2011@gmail.com>
2022-10-16 18:13:19 -0300

    feat: implementing example clear

---
+++ python/tipos_embutidos/clear_list.py
@@ -0,0 +1,8 @@
+"""
+Limpar a lista
+O método clear() esvazia a lista.
+A lista ainda permanece, mas não tem conteúdo.
+"""
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
