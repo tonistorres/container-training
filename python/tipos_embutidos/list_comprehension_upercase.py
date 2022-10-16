a607eaea67bc56aac9a2e25a7a35c35b97a36abf
tonistorres <sisvenda2011@gmail.com>
2022-10-16 18:14:24 -0300

    feat: implementing example list_comprehension

---
+++ python/tipos_embutidos/list_comprehension_upercase.py
@@ -0,0 +1,6 @@
+"""
+Defina os valores na nova lista para maiúsculas:
+"""
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x.upper() for x in fruits]
+print("Nova lista com caixa Alta List Comprehension:{}".format(newlist))
