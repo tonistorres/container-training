fc4a2314afe26857d172e475862498cc107c6533
tonistorres <sisvenda2011@gmail.com>
2022-10-16 18:13:58 -0300

    feat: implementing example list_comprehension

---
+++ python/tipos_embutidos/list_comprehension_1.py
@@ -0,0 +1,9 @@
+"""
+Loop usando compreensão de lista
+O List Comprehension oferece a sintaxe mais curta
+para percorrer listas:
+
+"""
+
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
