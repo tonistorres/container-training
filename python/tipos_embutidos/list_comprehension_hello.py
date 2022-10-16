4db2a4760fbbb1a9dfb6de2cf67fef3d08e07404
tonistorres <sisvenda2011@gmail.com>
2022-10-16 18:14:30 -0300

    feat: implementing example list_comprehension

---
+++ python/tipos_embutidos/list_comprehension_hello.py
@@ -0,0 +1,10 @@
+""""
+
+Defina todos os valores na nova lista para 'hello':
+
+"""
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = ["hello" for x in fruits]
+
+print("Redefinindo valor:{}".format(newlist))
