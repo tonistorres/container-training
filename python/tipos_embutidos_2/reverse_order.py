2661c5a6346961e52e0aad816b1acc34b11fa106
tonistorres <sisvenda2011@gmail.com>
2022-10-17 11:32:40 -0300

    feat: example method reverse

---
+++ python/tipos_embutidos_2/reverse_order.py
@@ -0,0 +1,12 @@
+"""
+E se você quiser inverter a ordem de uma lista,
+independentemente do alfabeto?
+
+O método reverse() inverte a ordem de classificação
+ atual dos elementos.
+
+"""
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+thislist.reverse()
+print(thislist)
