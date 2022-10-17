c8ad87eea917e3d36066fdbf530187bf16f9fc89
tonistorres <sisvenda2011@gmail.com>
2022-10-17 11:33:39 -0300

    feat: example method sort

---
+++ python/tipos_embutidos_2/sort_list.py
@@ -0,0 +1,13 @@
+"""
+Objetos de lista têm um método sort() que classificará
+a lista em ordem alfanumérica, crescente, por padrão:
+"""
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
+
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist)
