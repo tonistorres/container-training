fb10edd0e7406fcf4d2533598c7811455719bec2
tonistorres <sisvenda2011@gmail.com>
2022-10-17 11:33:24 -0300

    feat: example method sort_descending

---
+++ python/tipos_embutidos_2/sort_descending.py
@@ -0,0 +1,14 @@
+"""
+Para classificar de forma decrescente,
+use o argumento de palavra-chave reverse = True:
+
+"""
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse=True)
+print(thislist)
+
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse=True)
+print(thislist)
