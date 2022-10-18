09acf227c2cec085914ca9cf84fc60b5cedfcf7a
tonistorres <sisvenda2011@gmail.com>
2022-10-18 14:47:42 -0300

    feat: implementing examples built-in types

---
+++ python/tipos_embutidos_3/join_extend.py
@@ -0,0 +1,11 @@
+"""
+Ou você pode usar o método extend(),
+cuja finalidade é adicionar elementos
+de uma lista para outra lista
+"""
+
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list1.extend(list2)
+print(list1)
