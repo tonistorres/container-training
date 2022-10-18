eb2700f7dbfab8e22b750518e875d025a1a6d75b
tonistorres <sisvenda2011@gmail.com>
2022-10-18 14:48:25 -0300

    feat: implementing examples built-in types

---
+++ python/tipos_embutidos_4/extend.py
@@ -0,0 +1,18 @@
+"""
+
+Definição e uso
+O método extend() adiciona os elementos de lista
+ especificados (ou qualquer iterável) ao final da lista atual.
+
+Sintaxe
+list.extend(iterável)
+
+"""
+
+fruits = ["apple", "banana", "cherry"]
+
+points = (1, 4, 5, 9)
+
+fruits.extend(points)
+
+print(fruits)
