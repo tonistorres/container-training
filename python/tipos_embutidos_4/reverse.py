cb04cc710f9e3c17511608d2317998df7dcbb4b2
tonistorres <sisvenda2011@gmail.com>
2022-10-18 14:48:55 -0300

    feat: implementing examples built-in types

---
+++ python/tipos_embutidos_4/reverse.py
@@ -0,0 +1,13 @@
+"""
+Definição e uso
+O método reverse() inverte a ordem de classificação dos elementos.
+
+Sintaxe
+lista.reverso()
+
+"""
+
+fruits = ["apple", "banana", "cherry"]
+fruits.reverse()
+
+print("lista reversa: {}".format(fruits))
