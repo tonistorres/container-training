41ec762868d25fa21801385c5c39f74f3aee497d
tonistorres <sisvenda2011@gmail.com>
2022-10-18 14:47:27 -0300

    feat: implementing examples built-in types

---
+++ python/tipos_embutidos_3/copy_method_list.py
@@ -0,0 +1,9 @@
+"""
+Outra maneira de fazer uma cópia é usar o
+método interno list()
+"""
+# Faça uma cópia de uma lista com o método list()
+
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
