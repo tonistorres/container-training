77ff1866e5d4d34088835068cca7da631a6c0931
tonistorres <sisvenda2011@gmail.com>
2022-10-18 14:48:01 -0300

    feat: implementing examples built-in types

---
+++ python/tipos_embutidos_4/append.py
@@ -0,0 +1,13 @@
+"""
+Adicione um elemento à lista de frutas:
+vale apenas ressaltar que ser iserido no
+final da lista
+"""
+
+# Syntax
+# list.append(elmnt)
+
+
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+print(fruits)
