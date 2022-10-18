953eb0c2db63fff2bccbf1235ffe3e41e7f1b7e3
tonistorres <sisvenda2011@gmail.com>
2022-10-18 14:48:07 -0300

    feat: implementing examples built-in types

---
+++ python/tipos_embutidos_4/clear.py
@@ -0,0 +1,13 @@
+"""
+Remova todos os elementos da lista de frutas:
+"""
+# Definição e uso
+# O método clear() remove todos os elementos de uma lista.
+
+# Sintaxe
+# list.clear()
+
+fruits = ["apple", "banana", "cherry", "orange"]
+fruits.clear()
+
+print(f"Removendo todos os itens da lista de frutas ${fruits}")
