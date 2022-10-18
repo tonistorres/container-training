d93653953ab8381583807f4fbbb6212ef7940fd2
tonistorres <sisvenda2011@gmail.com>
2022-10-18 14:48:37 -0300

    feat: implementing examples built-in types

---
+++ python/tipos_embutidos_4/insert.py
@@ -0,0 +1,14 @@
+"""
+Definição e uso
+O método insert() insere o valor especificado na posição especificada.
+
+Sintaxe
+list.insert(pos, elmnt)
+
+"""
+
+fruits = ["apple", "banana", "cherry"]
+
+fruits.insert(1, "orange")
+
+print(f"Adiciona na posição 1 e empurra os demais da lista: {fruits}")
