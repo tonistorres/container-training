513d7422d2c804cde248cc687b53a948c3b95b4e
tonistorres <sisvenda2011@gmail.com>
2022-10-18 14:48:49 -0300

    feat: implementing examples built-in types

---
+++ python/tipos_embutidos_4/remove.py
@@ -0,0 +1,12 @@
+"""
+
+Definição e uso
+O método remove() remove a primeira ocorrência do elemento
+com o valor especificado.
+
+Sintaxe
+list.remove(elmnt)
+"""
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+print(f"Removendo banana: {fruits}")
