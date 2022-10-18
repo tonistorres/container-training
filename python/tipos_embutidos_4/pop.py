c1ac166629bb842bf4778814920ecd03d31b4d98
tonistorres <sisvenda2011@gmail.com>
2022-10-18 14:48:43 -0300

    feat: implementing examples built-in types

---
+++ python/tipos_embutidos_4/pop.py
@@ -0,0 +1,12 @@
+"""
+Definição e uso
+O método pop() remove o elemento na posição especificada.
+
+Sintaxe
+list.pop(pos)
+"""
+
+fruits = ["apple", "banana", "cherry"]
+x = fruits.pop(1)
+
+print(f"Removendo elemento na posição especificada: {fruits}")
