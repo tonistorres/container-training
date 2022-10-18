10cf5a60b1bed2f83016ab58bebd0a469c595677
tonistorres <sisvenda2011@gmail.com>
2022-10-18 14:47:49 -0300

    feat: implementing examples built-in types

---
+++ python/tipos_embutidos_3/join_two_list.py
@@ -0,0 +1,11 @@
+"""
+Existem várias maneiras de unir ou concatenar duas
+ou mais listas em Python.
+Uma das maneiras mais fáceis é usando o operador +.
+"""
+
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3)
