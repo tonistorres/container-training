11824569ee1017e023f9f4eb4d49f88a2abc037f
tonistorres <sisvenda2011@gmail.com>
2022-10-18 14:48:14 -0300

    feat: implementing examples built-in types

---
+++ python/tipos_embutidos_4/copy.py
@@ -0,0 +1,12 @@
+""""
+Definição e uso
+O método copy() retorna uma cópia da lista especificada.
+
+Sintaxe
+lista.copiar()
+"""
+
+fruits = ["apple", "banana", "cherry", "orange"]
+x = fruits.copy()
+
+print("Imprimindo a copia de fruits:{}".format(x))
