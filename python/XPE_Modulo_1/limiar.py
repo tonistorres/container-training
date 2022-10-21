5124e6128883d61b9aefbb0646a3fe879891aea6
tonistorres <sisvenda2011@gmail.com>
2022-10-21 20:27:02 -0300

    feat: implementing example tuple

---
+++ python/XPE_Modulo_1/limiar.py
@@ -0,0 +1,15 @@
+limiar = 5
+menores = []  # cria lista menores
+maiores = []  # cria lista maiores
+
+# divide os números de 1 a 10 em maiores e menores
+for i in range(10):
+    if i < limiar:
+        menores.append(i)
+    elif i > limiar:
+        maiores.append(i)
+
+# imprime na tela os valores das listas
+print("Resultado final")
+print("menores:", menores)
+print("maiores:", maiores)
