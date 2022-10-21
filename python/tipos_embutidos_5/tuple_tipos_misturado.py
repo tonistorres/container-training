5f702e881e00415f4e5f3f06bd85f5eee08ec087
tonistorres <sisvenda2011@gmail.com>
2022-10-21 20:26:42 -0300

    feat: implementing example tuple

---
+++ python/tipos_embutidos_5/tuple_tipos_misturado.py
@@ -0,0 +1,11 @@
+"""
+Uma tupla pode conter diferentes tipos de dados:
+
+Exemplo
+Uma tupla com strings, inteiros e valores booleanos
+"""
+
+tuple1 = ("abc", 34, True, 40, "male")
+
+print(f"Que tipo de dado é esse? {type(tuple1)}")
+print(f"Imprima o resultado desta estrutura {tuple1}")
