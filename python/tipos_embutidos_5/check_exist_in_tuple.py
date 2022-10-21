bb1e9a559ba9d3e5ebf8660482b83d52b9243c6c
tonistorres <sisvenda2011@gmail.com>
2022-10-21 20:25:39 -0300

    feat: implementing example tuple

---
+++ python/tipos_embutidos_5/check_exist_in_tuple.py
@@ -0,0 +1,8 @@
+"""
+Check if Item Exists
+To determine if a specified item is present in a tuple use the in keyword:
+"""
+
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+    print("Yes, 'apple' is in the fruits tuple")
