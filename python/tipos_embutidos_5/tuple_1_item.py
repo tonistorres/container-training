6d4ca424a315c8f2bc16edcc36abc0ee1a1b5008
tonistorres <sisvenda2011@gmail.com>
2022-10-21 20:25:46 -0300

    feat: implementing example tuple

---
+++ python/tipos_embutidos_5/tuple_1_item.py
@@ -0,0 +1,16 @@
+"""
+
+Criar Tupla Com Um Item
+Para criar uma tupla com apenas um item,
+ você deve adicionar uma vírgula após o item,
+ caso contrário, o Python não o reconhecerá como uma tupla.
+
+"""
+
+# tuple
+thistuple = ("apple",)
+print(type(thistuple))
+
+# NOT a tuple
+# thistuple = ("apple")
+# print(type(thistuple))
