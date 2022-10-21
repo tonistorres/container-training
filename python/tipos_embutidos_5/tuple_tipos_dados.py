804362a7aa9bb69775e9fba621395a137893501e
tonistorres <sisvenda2011@gmail.com>
2022-10-21 20:26:37 -0300

    feat: implementing example tuple

---
+++ python/tipos_embutidos_5/tuple_tipos_dados.py
@@ -0,0 +1,12 @@
+"""
+Itens de Tupla - Tipos de Dados
+Os itens de tupla podem ser de qualquer tipo de dados:
+"""
+
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+
+print(f"Tuple 1:{type(tuple1)}")
+print(f"Tuple 1:{type(tuple2)}")
+print(f"Tuple 1:{type(tuple3)}")
