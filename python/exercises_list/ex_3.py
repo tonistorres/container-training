0bf9c867b486e1af88b44d0c6e947dba08640336
tonistorres <sisvenda2011@gmail.com>
2022-10-21 20:21:58 -0300

    refactor: deleting files not used

--- python/exercises_list/ex_3.py
+++
@@ -1,7 +0,0 @@
-"""
-Use o método append para adicionar "laranja" à lista de frutas.
-"""
-
-fruits = ["apple", "banana", "cherry"]
-fruits.append("orange")
-print(fruits)
--- python/exercises_list/ex_8.py
+++
@@ -1,5 +0,0 @@
-"""
-Use a sintaxe correta para imprimir o número de itens na lista.
-"""
-fruits = ["apple", "banana", "cherry"]
-print(len(fruits))
