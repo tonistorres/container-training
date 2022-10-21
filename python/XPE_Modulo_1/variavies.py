5b6fd98e42230ea82b85a71bdb4c7245b7797348
tonistorres <sisvenda2011@gmail.com>
2022-10-21 20:27:14 -0300

    feat: implementing example tuple

---
+++ python/XPE_Modulo_1/variavies.py
@@ -0,0 +1,13 @@
+# atribuição por meio da declaração de um valor
+nome = "João"
+var_booleano = True
+raio = 2
+pi = 3.14159265359
+# atribuição por meio de uma expressão
+area = pi * (raio * raio)
+nome_completo = nome + "da Silva"
+media = (10 + 15 + 20 + 25) / 4
+linha = "***************************"
+print(
+    f"\n{linha}\nArea do Circulo:{area}\n Nome:{nome_completo}\n Media:{media}\n{linha}\n"
+)
