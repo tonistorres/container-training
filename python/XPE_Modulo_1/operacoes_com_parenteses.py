52f6979a915f53189b8e651f19ea493ea22e9826
tonistorres <sisvenda2011@gmail.com>
2022-10-21 20:27:09 -0300

    feat: implementing example tuple

---
+++ python/XPE_Modulo_1/operacoes_com_parenteses.py
@@ -0,0 +1,9 @@
+x = 2 * (3 + 4)
+y = (5 - 2) * (3 + 4)
+z = (x + y) * 3
+a = (x + y) / z
+
+print("Valor de x:{}".format(x))
+print("Valor de y:{}".format(y))
+print("Valor de z:{}".format(z))
+print("Valor de a:%.2f" % a)
