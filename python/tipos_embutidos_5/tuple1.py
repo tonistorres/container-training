b429ee32abc0c5192f54ec1071f807f39df6bb09
tonistorres <sisvenda2011@gmail.com>
2022-10-21 20:26:51 -0300

    feat: implementing example tuple

---
+++ python/tipos_embutidos_5/tuple1.py
@@ -0,0 +1,14 @@
+"""
+Uma tupla é uma coleção ordenada e imutável.
+
+Tuplas são escritas com colchetes.
+
+
+Comprimento da Tupla
+Para determinar quantos itens uma tupla possui, use a função len():
+"""
+
+thistuple = ("apple", "banana", "cherry")
+tipo = type(thistuple)
+print(f"Qual tipo de estrutura?{tipo}")
+print(len(thistuple))
