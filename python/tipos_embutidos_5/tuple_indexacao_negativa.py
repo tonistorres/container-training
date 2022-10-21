b97e2c9ec6360e8a67a89f3f3903dc2ead9766ef
tonistorres <sisvenda2011@gmail.com>
2022-10-21 20:26:06 -0300

    feat: implementing example tuple

---
+++ python/tipos_embutidos_5/tuple_indexacao_negativa.py
@@ -0,0 +1,12 @@
+"""
+
+Indexação negativa
+Indexação negativa significa começar do fim.
+
+-1 refere-se ao último item, -2 refere-se ao penúltimo item etc.
+
+"""
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-1])
+print(thistuple[-2])
