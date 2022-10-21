7bb240b1fde2f0efb586feb752f89eea49a4363d
tonistorres <sisvenda2011@gmail.com>
2022-10-21 20:26:25 -0300

    feat: implementing example tuple

---
+++ python/tipos_embutidos_5/tuple_range_intervalo.py
@@ -0,0 +1,6 @@
+"""
+Este exemplo retorna os itens do início para, mas NÃO inclui, "kiwi":
+"""
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[:4])
