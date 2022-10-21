265109d5de48e4a57f6798897779bf3c7d277dbc
tonistorres <sisvenda2011@gmail.com>
2022-10-21 20:26:01 -0300

    feat: implementing example tuple

---
+++ python/tipos_embutidos_5/tuple_construtor.py
@@ -0,0 +1,8 @@
+"""
+O construtor tupla()
+Também é possível usar o construtor tuple() para fazer uma tupla.
+"""
+
+
+thistuple = tuple(("apple", "banana", "cherry"))  # note the double round-brackets
+print(thistuple)
