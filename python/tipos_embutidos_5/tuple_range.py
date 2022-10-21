4f86cdb1351ba2da61dd2fae2104160e1560f959
tonistorres <sisvenda2011@gmail.com>
2022-10-21 20:26:31 -0300

    feat: implementing example tuple

---
+++ python/tipos_embutidos_5/tuple_range.py
@@ -0,0 +1,13 @@
+"""
+Faixa de índices
+Você pode especificar um intervalo de índices especificando onde começar e onde terminar o intervalo.
+
+Ao especificar um intervalo, o valor de retorno será uma nova tupla com os itens especificados.
+
+Nota: A pesquisa começará no índice 2 (incluído) e terminará no índice 5 (não incluído).
+
+Lembre-se que o primeiro item tem índice 0.
+"""
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
