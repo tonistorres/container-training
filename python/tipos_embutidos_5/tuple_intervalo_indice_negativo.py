f023c25c46ab5fa32ba58b8028375712b7ea613d
tonistorres <sisvenda2011@gmail.com>
2022-10-21 20:26:12 -0300

    feat: implementing example tuple

---
+++ python/tipos_embutidos_5/tuple_intervalo_indice_negativo.py
@@ -0,0 +1,7 @@
+"""
+Faixa de índices negativos
+Especifique índices negativos se desejar iniciar a pesquisa a partir do final da tupla:
+"""
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
