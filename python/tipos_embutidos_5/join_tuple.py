4b1c3c85068b78d39d619e59ea40efcf5e367be5
tonistorres <sisvenda2011@gmail.com>
2022-10-22 21:41:27 -0300

    feat: implementing example tuple

---
+++ python/tipos_embutidos_5/join_tuple.py
@@ -0,0 +1,10 @@
+"""
+Junte-se a duas tuplas
+Para juntar duas ou mais tuplas você pode usar o operador +
+"""
+
+tuple1 = ("a", "b", "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
