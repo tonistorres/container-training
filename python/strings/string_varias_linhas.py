f0862b9d200db811de7afee76b18a3cf86a2c799
tonistorres <sisvenda2011@gmail.com>
2022-10-22 21:40:05 -0300

    feat: implementing example string

---
+++ python/strings/string_varias_linhas.py
@@ -0,0 +1,17 @@
+"""
+Strings de várias linhas
+Você pode atribuir uma string de várias linhas a uma variável usando três aspas:
+"""
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+
+b = '''Lorem ipsum dolor sit bmet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(b)
