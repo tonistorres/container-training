ec6ee08d31f4dd8ecf0c8328dd02be4a86584d06
tonistorres <sisvenda2011@gmail.com>
2022-10-22 21:39:42 -0300

    feat: implementing example string

---
+++ python/strings/string_fatiamento.py
@@ -0,0 +1,9 @@
+"""
+Fatiamento
+Você pode retornar um intervalo de caracteres usando a sintaxe de fatia.
+Especifique o índice inicial e o índice final, separados por dois pontos,
+para retornar uma parte da string.
+"""
+
+b = "Hello, World!"
+print(b[2:5])
