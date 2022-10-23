675cd76b5f46de6de003de984868c81ddf40ce48
tonistorres <sisvenda2011@gmail.com>
2022-10-22 21:40:10 -0300

    feat: implementing example string

---
+++ python/strings/string_variavel.py
@@ -0,0 +1,8 @@
+"""
+Atribuir String a uma Variável
+A atribuição de uma string a uma variável é feita com o nome da
+variável seguido por um sinal de igual e a string:
+"""
+
+a = "Hello"
+print(a)
