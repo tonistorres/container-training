a356bd8dc659567961b175c1f99e8119be36417c
tonistorres <sisvenda2011@gmail.com>
2022-10-22 21:42:16 -0300

    feat: implementing example multiplicar

---
+++ python/tipos_embutidos_5/multiplicar_tuple.py
@@ -0,0 +1,11 @@
+"""
+
+Multiplicar Tuplas
+Se você deseja multiplicar o conteúdo de uma tupla um
+determinado número de vezes, pode usar o operador *:
+
+"""
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+
+print(mytuple)
