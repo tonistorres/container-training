9a0fea9501a75d2662bf7ffb8c809c22e231aad3
tonistorres <sisvenda2011@gmail.com>
2022-10-22 21:41:54 -0300

    feat: implementing example loop tuple element

---
+++ python/tipos_embutidos_5/loop_tuple_element.py
@@ -0,0 +1,10 @@
+"""
+
+Percorrer uma Tupla
+Você pode percorrer os itens da tupla usando um loop for
+
+"""
+
+thistuple = ("apple", "banana", "cherry")
+for element in thistuple:
+    print(element)
