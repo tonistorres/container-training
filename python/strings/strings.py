05f5c825ade8d30ae10ed3d96aa54e471a5de9f3
tonistorres <sisvenda2011@gmail.com>
2022-10-22 21:40:16 -0300

    feat: implementing example string

---
+++ python/strings/strings.py
@@ -0,0 +1,10 @@
+"""
+Strings em python são cercadas por aspas simples ou aspas duplas.
+
+'olá' é o mesmo que "olá" .
+
+Você pode exibir um literal de string com a print()função:
+"""
+
+print("Hello")
+print("Hello")
