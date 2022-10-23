926fd359d208482b78ce40344bf48c9799f4571f
tonistorres <sisvenda2011@gmail.com>
2022-10-22 21:40:47 -0300

    feat: implementing example tuple

---
+++ python/tipos_embutidos_5/tuple_metodo_count.py
@@ -0,0 +1,11 @@
+"""
+Definição e uso
+O método count() retorna o número de vezes que um valor especificado aparece na tupla.
+
+"""
+
+thistuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
+
+x = thistuple.count(5)
+
+print(x)
