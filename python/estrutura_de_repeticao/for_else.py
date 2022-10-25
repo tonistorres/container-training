7dbf0d979e6ec4bd98927d01afbdb70a5351087d
tonistorres <sisvenda2011@gmail.com>
2022-10-25 10:29:08 -0300

    feat: implementing example for

---
+++ python/estrutura_de_repeticao/for_else.py
@@ -0,0 +1,9 @@
+"""
+A elsepalavra-chave em um forloop especifica um bloco de código
+a ser executado quando o loop for concluído:
+Imprima todos os números de 0 a 5 e imprima uma mensagem quando o loop terminar:
+"""
+for x in range(6):
+    print(x)
+else:
+    print("Finally finished!")
