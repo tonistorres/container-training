54d031020231c99038dc716baa2bc07cd235a9bf
tonistorres <sisvenda2011@gmail.com>
2022-10-25 10:30:01 -0300

    feat: implementing example string

---
+++ python/strings/concatenacao/concat_1.py
@@ -0,0 +1,12 @@
+"""
+Concatenação de String
+Para concatenar ou combinar duas strings você pode usar o operador +.
+
+Mesclar variável acom variável bem variável c:
+
+"""
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
