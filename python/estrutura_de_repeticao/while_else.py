55af1d3eab6c257a8972b78275d1b11a4b4e2540
tonistorres <sisvenda2011@gmail.com>
2022-10-25 10:29:46 -0300

    feat: implementing example while

---
+++ python/estrutura_de_repeticao/while_else.py
@@ -0,0 +1,12 @@
+"""
+A declaração else
+Com a instrução else podemos executar um bloco de código uma vez
+quando a condição não for mais verdadeira:
+
+"""
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("i is no longer less than 6")
