e016cce09683787becd192ee9d7a5eb38446b50b
tonistorres <sisvenda2011@gmail.com>
2022-10-25 10:28:49 -0300

    feat: implementing example for

---
+++ python/estrutura_de_repeticao/for_break.py
@@ -0,0 +1,16 @@
+"""
+Com a instrução break podemos parar o loop antes que
+ele tenha percorrido todos os itens:
+"""
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#     print(x)
+#     if x == "banana":
+#         break
+
+
+fruits_2 = ["apple", "banana", "cherry"]
+for y in fruits_2:
+    if y == "banana":
+        break
+    print(y)
