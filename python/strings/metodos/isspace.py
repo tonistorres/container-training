1222eb124a18ce913f11c5f06bffab5cfd646812
tonistorres <sisvenda2011@gmail.com>
2022-10-27 11:38:23 -0300

    feat: implementing example code isspace

---
+++ python/strings/metodos/isspace.py
@@ -0,0 +1,22 @@
+"""
+Definição e uso
+O isspace()método retorna True se todos os caracteres em uma string
+forem espaços em branco, caso contrário, False.
+
+Sintaxe
+string.isspace()
+
+"""
+
+txt = "   "
+
+x = txt.isspace()
+
+print(x)
+
+
+tyt = "   s   "
+
+y = tyt.isspace()
+
+print(y)
