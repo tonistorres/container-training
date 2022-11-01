eb2d23d36e3735d13b6fc4218284a27ba965e6da
tonistorres <sisvenda2011@gmail.com>
2022-11-01 09:11:14 -0300

    feat: implementing shell script

---
+++ python/file_manipulation/open/execute/modules/i.sh
@@ -0,0 +1,89 @@
+MENU_ACCES="
+----------------------------------------------------
+Acessando um site na iternet para isso utilizaremos
+uma dependência
+---------------------------------------------------
+"
+
+MENU_ISNTALL_ENVIRONMENT="
+----------------------------------------------------
+Criando um Ambiente Virtual
+---------------------------------------------------
+"
+
+
+
+TIME(){
+echo "In 2s"
+sleep 1
+echo "In 1s"
+sleep 1
+}
+
+CRAIANDOAMBIENTEVIRTUAL(){
+echo "$MENU_ISNTALL_ENVIRONMENT"
+TIME
+# deactivate
+python3 -m venv .venv && source .venv/bin/activate
+}
+
+INSTALL_COLORAMA(){
+  pip install colorama
+}
+
+INSTALL_OS(){
+  pip install os
+}
+
+
+INSTALANDOBLACKFLAKE(){
+echo "Install Black"
+pip3 install black
+TIME
+echo "Install Flake8"
+pip3 install flake8
+TIME
+}
+
+MOSTRARPACOTESINSTALADOS(){
+  pip3 freeze
+}
+
+# https://bobbyhadz.com/blog/python-no-module-named-mysql
+INTALL_CONNECTOR_MYSQ(){
+pip3 install mysql-connector-python
+}
+
+ACESSANDOSITEINTERNET(){
+echo "$MENU_ACCES"
+INSTALANDOBLACKFLAKE
+source .venv/bin/activate
+which python3
+}
+
+
+GITIGNORE(){
+touch .gitignore
+cat << EOF >> .gitignore
+.venv/
+__pycache__
+
+EOF
+}
+
+INSTALADORES(){
+python3 -m pip install --upgrade pip
+CRAIANDOAMBIENTEVIRTUAL
+ACESSANDOSITEINTERNET
+GITIGNORE
+INSTALL_COLORAMA
+INSTALL_OS
+MOSTRARPACOTESINSTALADOS
+}
+
+
+MAIN(){
+INSTALADORES
+}
+
+MAIN
