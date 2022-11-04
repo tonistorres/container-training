504079597a67f9b1d7212cf12abcdecf1af19608
tonistorres <sisvenda2011@gmail.com>
2022-11-03 22:12:58 -0300

    feat: working library numpy

---
+++ python/Numpy/create_ndarray.py
@@ -0,0 +1,16 @@
+"""
+Criar um objeto NumPy ndarray
+NumPy é usado para trabalhar com arrays. O objeto array no NumPy
+é chamado ndarray.
+
+Podemos criar um ndarrayobjeto NumPy usando a array()função.
+Nota: type(): Essa função interna do Python nos informa o tipo do
+objeto passado para ela. Como no código acima, mostra que arré numpy.
+ndarray tipo.
+
+"""
+import numpy as np
+
+arr = np.array(["marina", "da", "silva"])
+
+print(type(arr))
