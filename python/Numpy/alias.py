cf68ecee6eefe908d1a4c7c36086a8a7e10a1d17
tonistorres <sisvenda2011@gmail.com>
2022-11-03 22:12:37 -0300

    feat: working library numpy

---
+++ python/Numpy/alias.py
@@ -0,0 +1,14 @@
+"""
+NumPy como np
+NumPy geralmente é importado sob o np alias.
+alias: em Python, alias é um nome alternativo para se referir
+à mesma coisa.
+Crie um alias com a palavra- aschave durante a importação:
+"""
+
+import numpy as np
+
+# Agora, o pacote NumPy pode ser referido como npem vez de numpy.
+
+array_alias = np.array(["ana", "paula"])
+print(array_alias)
