c9be075338f5ec2b97ec3f700e7598bae62fb5ea
tonistorres <sisvenda2011@gmail.com>
2022-11-03 22:13:09 -0300

    feat: working library numpy

---
+++ python/Numpy/matriz_0d.py
@@ -0,0 +1,18 @@
+"""
+Dimensões em matrizes
+Uma dimensão em matrizes é um nível de profundidade de matriz
+(matrizes aninhadas).
+
+array aninhado: são arrays que possuem arrays como seus elementos.
+
+
+"""
+
+# Crie uma matriz 0-D com valor 42
+import numpy as np
+
+
+arr = np.array(42)
+
+print(arr)
+print(type(arr))
