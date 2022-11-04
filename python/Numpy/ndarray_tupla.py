160d53bea8f9a3a7d712a1a003929ea2d7f6b3ed
tonistorres <sisvenda2011@gmail.com>
2022-11-03 22:14:42 -0300

    feat: working library numpy

---
+++ python/Numpy/ndarray_tupla.py
@@ -0,0 +1,13 @@
+"""
+Para criar um ndarray, podemos passar uma lista,
+tupla ou qualquer objeto do tipo array para o array() método,
+e ele será convertido em um ndarray:
+"""
+
+import numpy as np
+
+
+arr = np.array((1, 2, 3, 4, 5))
+
+print(arr)
+print(type(arr))
