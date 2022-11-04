9c0ea6d63607fd879f24d3a43c1b48135bca1ffc
tonistorres <sisvenda2011@gmail.com>
2022-11-03 22:13:55 -0300

    feat: working library numpy

---
+++ python/Numpy/matriz_3d.py
@@ -0,0 +1,10 @@
+"""
+Crie um array 3D com dois arrays 2D, ambos contendo dois arrays
+com os valores 1,2,3 e 4,5,6:
+
+"""
+import numpy as np
+
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+print(arr)
