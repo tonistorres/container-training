f11d001ca06c5cb96a1499752ccea86d1f6464d0
tonistorres <sisvenda2011@gmail.com>
2022-11-09 19:05:07 -0300

    feat: implementing example Numpy

---
+++ python/Numpy/sliece_3.py
@@ -0,0 +1,7 @@
+# Fatiar elementos do início ao índice 4 (não incluído):
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+
+print(f"Fatiando do índice 4 ao inicio do array:\n{arr[:4]}")
