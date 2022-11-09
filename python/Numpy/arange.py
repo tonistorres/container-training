adaccadd746e107c90c2fcd23837ddb2a16abdda
tonistorres <sisvenda2011@gmail.com>
2022-11-09 19:03:19 -0300

    feat: implementing example Numpy

---
+++ python/Numpy/arange.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# criar array a partir de faixa de valores :metodo arange
+# Array de 10 elementos números de 0 a 9
+print(np.arange(10))
+
+# Gerar uma array de float de 10 elementos números 0 a 9
+print(np.arange(10.0))
+
+# Gerar um array no intervalo de 8 a 20
+print(np.arange(8, 21))
+
+# Gerar um array de 50 até 1 números pares e decrescente
+print(np.arange(50,1,-2))
