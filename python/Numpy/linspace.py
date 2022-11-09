4987d221f207481bdeb541a8819df2bf3712f844
tonistorres <sisvenda2011@gmail.com>
2022-11-09 19:03:55 -0300

    feat: implementing example Numpy

---
+++ python/Numpy/linspace.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# método linspace cria array com valores de ponto
+# flutuante
+# primeiro argumento =  valor inicial
+# segundo  argumento = valor final
+# terceiro argumento = quantidade de itens a gerar
+# observação: não são numéros aleatórios e sim números
+# igualmente espaçados
+
+print(np.linspace(0.0, 2.0, num=5))
