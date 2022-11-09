b4958ede9eb65f3fabf32270e25656951ffe7458
tonistorres <sisvenda2011@gmail.com>
2022-11-09 19:05:58 -0300

    feat: implementing example Numpy

---
+++ python/Numpy/xpe_create_zeros_2d_3.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+dim = (2, 2)  # linhas e colunas
+a = np.zeros(dim)
+
+
+print(
+    f"""-----------------------------------------------
+   Analysis ndArray Report Versão Numpy {np.__version__}
+-----------------------------------------------
+1- Imprimindo o Array:\n{a}
+2- Número de Linhas e Colunas do Array:{a.shape} e Nº Dimenssões:{a.ndim}
+3- Qual Tipo do Array:{type(a)}
+4- Qual Tipo de elementos contidos nesse Array:{a.dtype}
+5- Número de Elementos contidos na Matriz:{a.size}
+6- Acesse o Elemento na PRIMEIRA LINHA e SEGUNDA COLUNA:{a[0,0]}
+7- Acesse o Elemento da SEGUNDA LINHA e QUARTA COLUNA:{a[1,1]}
+8- Soma dos Elementos do Item 6 e 7:{a[0,0]+a[1,1]}
+"""
+)
