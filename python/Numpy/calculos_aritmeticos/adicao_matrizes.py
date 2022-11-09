3fb7a8c95b438fd5fba961ba066ea8b01700cb8e
tonistorres <sisvenda2011@gmail.com>
2022-11-09 19:06:36 -0300

    feat: implementing example Numpy

---
+++ python/Numpy/calculos_aritmeticos/adicao_matrizes.py
@@ -0,0 +1,14 @@
+# Adição
+# A add()função soma o conteúdo de duas matrizes e retorna os resultados em uma nova matriz.
+
+# Exemplo
+# Adicione os valores em arr1 aos valores em arr2:
+
+import numpy as np
+
+arr1 = np.array([10, 11, 12, 13, 14, 15])
+arr2 = np.array([20, 21, 22, 23, 24, 25])
+
+newarr = np.add(arr1, arr2)
+
+print(newarr)
