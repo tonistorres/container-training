36efceb1c48f6ea9830af6cef277eccfbcae9e21
tonistorres <sisvenda2011@gmail.com>
2022-11-09 19:07:02 -0300

    feat: implementing example Numpy

---
+++ python/Numpy/calculos_aritmeticos/resto_divisao_2.py
@@ -0,0 +1,13 @@
+# Você obtém o mesmo resultado ao usar a remainder()função:
+
+# Exemplo
+# Devolva os restos:
+
+import numpy as np
+
+arr1 = np.array([10, 20, 30, 40, 50, 60])
+arr2 = np.array([3, 7, 9, 8, 2, 33])
+
+newarr = np.remainder(arr1, arr2)
+
+print(newarr)
