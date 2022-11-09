8487e906dd936235575db00da68b66c2a563ae1c
tonistorres <sisvenda2011@gmail.com>
2022-11-09 19:04:07 -0300

    feat: implementing example Numpy

---
+++ python/Numpy/rand.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# criar e preencher um array com valores aleatórios
+# Array de 10 elementos entre 0 e 1
+a = np.random.rand(10)
+print(f"Array a:\n{a}")
+# Gerar um array bidimenssional com número aleatorio
+# Array 3x3
+b = np.random.rand(3, 3)
+print(f"Array b:\n{b}")
