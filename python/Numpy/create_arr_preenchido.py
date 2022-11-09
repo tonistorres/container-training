34f8f6bc09f981858a97137f9cb16548a52399b8
tonistorres <sisvenda2011@gmail.com>
2022-11-09 19:03:26 -0300

    feat: implementing example Numpy

---
+++ python/Numpy/create_arr_preenchido.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+print("Criando array preenchendo com 0 (zeros):\n", np.zeros(10))
+print("Criando array definindo tipo com 0 (zeros):\n", np.zeros(10, dtype=int))
+print("Criando array preenchendo com numero 1 (um): \n", np.ones(15))
+print("Criando array definindo tipo com numero 1 (um): \n", np.ones(15, dtype=int))
+print("Criando array preenchendo com numero específico:\n", np.full(8, 12))
+print("Criando e preenchendo um array 2d com 0(zeros)\n", np.zeros((2, 5), dtype=int))
+print("Criando e preenchendo um array 3d com 1(um)\n", np.ones((2, 5, 6), dtype=int))
