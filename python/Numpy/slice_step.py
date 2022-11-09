3d91c383875726cca807e9842e9716d02a68e796
tonistorres <sisvenda2011@gmail.com>
2022-11-09 19:05:02 -0300

    feat: implementing example Numpy

---
+++ python/Numpy/slice_step.py
@@ -0,0 +1,11 @@
+# DEGRAU
+# Use o stepvalor para determinar a etapa do fatiamento:
+
+# Exemplo
+# Retorne todos os outros elementos do índice 1 ao índice 5:
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+
+print(arr[1:5:2])
