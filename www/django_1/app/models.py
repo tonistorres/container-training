4a7aa667ef78614db894f874bbc0c276fd932644
tonistorres <sisvenda2011@gmail.com>
2022-11-28 09:14:20 -0300

    feat: adding py in project

--- www/django_1/app/models.py
+++ www/django_1/app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    nome = models.CharField("nome", max_length=60)
+    celular = models.BigIntegerField("celular")
