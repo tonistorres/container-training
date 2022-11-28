8ea7382fc96af6d010d76227a9c1f3264168f2be
tonistorres <sisvenda2011@gmail.com>
2022-11-28 09:35:41 -0300

    feat: adding file shell in project

---
+++ www/projeto-todo/run.sh
@@ -0,0 +1,21 @@
+TIME(){
+  sleep 1
+  echo '2s'
+  sleep 1
+  echo '1s'
+}
+
+RODANDO_PROJETO(){
+TIME
+echo '**************************'
+echo 'Rodando Projeto Django'
+echo '**************************'
+python manage.py runserver
+
+}
+
+MAIN(){
+RODANDO_PROJETO
+}
+
+MAIN
